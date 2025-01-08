e6d924d120147a89287867b163043aad38c3f766
hecate <105847046+roo2k@users.noreply.github.com>
2025-01-08 13:41:35 +0500

    gettooldver

--- htdocs/lib/db.php
+++
@@ -1,2 +0,0 @@
-<?php
-$pd0 = new PDO('mysql:host=localhost; dbname=php-website; port=3306', 'root', 'root');
--- htdocs/logout.php
+++
@@ -1,3 +0,0 @@
-<?php 
-setcookie("login", "", time()-3600);
-header('Location: /index.php');
